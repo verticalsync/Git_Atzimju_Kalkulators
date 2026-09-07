df3a40993e955808e10721571d07cd6b18ee10da
verticalsync <vsync@riseup.net>
2026-09-07 10:47:41 +0300

    feat: studentu skaitu ievadīšana

--- src/cc/vsync/atzimjukalkulators/Main.java
+++ src/cc/vsync/atzimjukalkulators/Main.java
@@ -1,10 +1,20 @@
 package cc.vsync.atzimjukalkulators;
 
+import java.util.Scanner;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int stSkaits;
+		System.out.print("Ievadiet studentu skaitu: ");
+		while (!sc.hasNextInt()) {
+			System.out.print("Ievadiet studentu skaitu: ");
+			sc.next();
+		}
 
+		stSkaits = sc.nextInt();
+		
+		
+		sc.close();
 	}
-
 }
