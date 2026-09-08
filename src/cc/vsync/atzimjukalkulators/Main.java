0ae737ab787a8d06324ca797e036a2f552dcd86c
verticalsync <vsync@riseup.net>
2026-09-08 10:39:27 +0300

    feat: Videjas atzimes rekinasana

--- src/cc/vsync/atzimjukalkulators/Main.java
+++ src/cc/vsync/atzimjukalkulators/Main.java
@@ -37,21 +37,31 @@ public class Main {
 		return r;
 	}
 	
+	static double videjaAtzime(int[] atzimes, int[] svars) {
+		double rezultats = 0;
+		
+		for (int i = 0; i < atzimes.length; i++) {
+			rezultats += (double)(atzimes[i] * svars[i]) / 100;
+		}
+		
+		return rezultats;
+	}
+	
 	public static void main(String[] args) {
 		int stSkaits = askInt("Ievadiet studentu skaitu: ", 1, 100);
 		int krSkaits = askInt("Ievadiet kritēriju skaitu: ", 1, 20);
 		
 		String[] studenti = new String[stSkaits];
-		
-		for (int i = 0; i < stSkaits; i++) {
-			studenti[i] = askString("Ievadiet " + (i+1) + ". studenta vārdu: ", 3, 16);
-		}
-		
 		String[] kriterijas = new String[krSkaits];
 		int[] kriterijasSvars = new int[krSkaits];
 		int[][] atzimes = new int[stSkaits][krSkaits];
 		int totalSvars = 0;
 		
+		for (int i = 0; i < stSkaits; i++) {
+			studenti[i] = askString("Ievadiet " + (i+1) + ". studenta vārdu: ", 3, 16);
+		}
+		
+		System.out.println();
 		for (int i = 0; i < krSkaits; i++) {
 			kriterijas[i] = askString("Ievadiet " + (i+1) + ". kritēriju: ", 1, 100);
 			kriterijasSvars[i] = askInt("Ievadiet '" + kriterijas[i] + "' kritērijas svaru (5-100): ", 5, 100);
@@ -59,10 +69,11 @@ public class Main {
 		}
 		
 		if (totalSvars != 100) {
-			System.out.println("Visu kritēriju svaram kopsumma ir jābūt 100%, ne vairak, ne mazāk!");
+			System.out.println("\nVisu kritēriju svaram kopsumma ir jābūt 100%, ne vairak, ne mazāk!");
 			return;
 		}
 		
+		System.out.println();
 		for (int i = 0; i < stSkaits; i++) {
 			System.out.printf("Ievadiet balles (1-10) studentam %s\n", studenti[i]);
 			for (int j = 0; j < krSkaits; j++) {
@@ -70,7 +81,10 @@ public class Main {
 			}
 		}
 		
-		
+		System.out.println();
+		for (int i = 0; i < stSkaits; i++) {
+			System.out.printf("Studenta %s vidējā atzīme: %.2f", studenti[i], videjaAtzime(atzimes[i], kriterijasSvars));
+		}
 		
 		sc.close();
 	}
