a5efec0340d0ab9bda5480c98affadb252ed8e94
verticalsync <vsync@riseup.net>
2026-09-18 09:13:16 +0300

    feat: finalize menu and its functionality

--- src/cc/vsync/atzimjukalkulators/Main.java
+++ src/cc/vsync/atzimjukalkulators/Main.java
@@ -4,35 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 	static Scanner sc = new Scanner(System.in);
+	static String[] studenti;
+	static String[] kriterijas;
+	static int[] kriterijasSvars;
+	static int[][] atzimes;
+	static int totalSvars = 0;
 	
-	public static void main(String[] args) {
-		do{
-			System.out.println("Izvele:");
-			System.out.println("1 - Ievadit studentus");
-			System.out.println("2 - Ievadīt kriterijus");
-			System.out.println("3 - Ievadīt studentu atzīmes un redzēt vidējo");
-			System.out.println("4 - Beigt");
-			int izvele = Util.askInt(sc, "-> ", 1, 4);
-			switch (izvele) {
-				case 1:
-
-			}
-		}while (izvele != 4);
-
+	public static void inputStudents() {
 		int stSkaits = Util.askInt(sc, "Ievadiet studentu skaitu: ", 1, 100);
-		int krSkaits = Util.askInt(sc, "Ievadiet kritēriju skaitu: ", 1, 20);
-		
-		String[] studenti = new String[stSkaits];
-		String[] kriterijas = new String[krSkaits];
-		int[] kriterijasSvars = new int[krSkaits];
-		int[][] atzimes = new int[stSkaits][krSkaits];
-		int totalSvars = 0;
+		studenti = new String[stSkaits];
 		
 		for (int i = 0; i < stSkaits; i++) {
 			studenti[i] = Util.askString(sc, "Ievadiet " + (i+1) + ". studenta vārdu: ", 3, 16);
 		}
+	}
+	
+	public static void inputCriteria() {
+		int krSkaits = Util.askInt(sc, "Ievadiet kritēriju skaitu: ", 1, 20);
+		kriterijas = new String[krSkaits];
+		kriterijasSvars = new int[krSkaits];
+		totalSvars = 0;
 		
-		System.out.println();
 		for (int i = 0; i < krSkaits; i++) {
 			kriterijas[i] = Util.askString(sc, "Ievadiet " + (i+1) + ". kritēriju: ", 1, 100);
 			kriterijasSvars[i] = Util.askInt(sc, "Ievadiet '" + kriterijas[i] + "' kritērijas svaru (5-100): ", 5, 100);
@@ -43,19 +35,45 @@ public class Main {
 			System.out.println("\nVisu kritēriju svaru kopsummai ir jābūt 100%, ne vairāk, ne mazāk!");
 			return;
 		}
+	}
+	
+	public static void calculateGrades() {
+		atzimes = new int[studenti.length][kriterijas.length];
 		
-		System.out.println();
-		for (int i = 0; i < stSkaits; i++) {
+		for (int i = 0; i < studenti.length; i++) {
 			System.out.printf("Ievadiet balles (1-10) studentam %s\n", studenti[i]);
-			for (int j = 0; j < krSkaits; j++) {
+			for (int j = 0; j < kriterijas.length; j++) {
 				atzimes[i][j] = Util.askInt(sc, kriterijas[j] + " balles: ", 1, 10);
 			}
 		}
 		
 		System.out.println();
-		for (int i = 0; i < stSkaits; i++) {
-			System.out.printf("Studenta %s vidējā atzīme: %.2f", studenti[i], Util.videjaAtzime(atzimes[i], kriterijasSvars));
+		for (int i = 0; i < studenti.length; i++) {
+			System.out.printf("Studenta %s vidējā atzīme: %.2f\n", studenti[i], Util.videjaAtzime(atzimes[i], kriterijasSvars));
 		}
+	}
+	
+	public static void main(String[] args) {
+		int izvele = 0;
+		
+		do {
+			System.out.println("Izvele:");
+			System.out.println("1 - Ievadit studentus");
+			System.out.println("2 - Ievadīt kriterijus");
+			System.out.println("3 - Ievadīt studentu atzīmes un redzēt vidējo");
+			System.out.println("4 - Beigt");
+			
+			izvele = Util.askInt(sc, "-> ", 1, 4);
+			
+			switch (izvele) {
+				case 1: inputStudents(); break;
+				case 2: inputCriteria(); break;
+				case 3: calculateGrades(); break;
+				default: break;
+			}
+			
+			System.out.println();
+		} while (izvele != 4);
 		
 		sc.close();
 	}
